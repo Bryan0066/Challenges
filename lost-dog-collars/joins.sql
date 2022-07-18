a464b47c2dd70872fcf09e34f65201925478ceed
Bryan0066 <bgenest96@gmail.com>
2022-07-17 20:45:12 -0400

    finished hw sql

--- lost-dog-collars/joins.sql
+++ lost-dog-collars/joins.sql
@@ -1,1 +1,30 @@
-# YOUR SQL CODE GOES HERE
+SELECT dog_owners.name, lost_dog_collars.dog_name
+  FROM dog_owners
+  JOIN lost_dog_collars
+ON (dog_owners.dog_name = lost_dog_collars.dog_name);
+
+# 2
+SELECT dog_owners.name, lost_dog_collars.dog_name
+FROM dog_owners
+RIGHT JOIN lost_dog_collars
+ON (dog_owners.dog_name = lost_dog_collars.dog_name)
+WHERE dog_owners.name IS NULL ;
+
+# 3 
+SELECT dog_owners.name, lost_dog_collars.dog_name
+    FROM dog_owners
+    RIGHT JOIN lost_dog_collars
+ON (dog_owners.dog_name = lost_dog_collars.dog_name);
+
+# 4
+SELECT dog_owners.name, lost_dog_collars.dog_name
+    FROM dog_owners
+    LEFT JOIN lost_dog_collars
+ON (dog_owners.dog_name = lost_dog_collars.dog_name);
+
+_________
+SELECT dog_owners.name, lost_dog_collars.dog_name
+FROM dog_owners
+RIGHT JOIN lost_dog_collars
+ON (dog_owners.dog_name = lost_dog_collars.dog_name)
+ORDER BY dog_owners.name;
