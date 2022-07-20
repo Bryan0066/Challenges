15cea2c4b76d9731cffe4bb8f6fc46851eb65240
Bryan0066 <bgenest96@gmail.com>
2022-07-20 10:03:02 -0400

    hw submit

--- Week5/news-aggregator-postgresql/server.rb
+++ Week5/news-aggregator-postgresql/server.rb
@@ -46,32 +46,35 @@ post '/articles/new' do
   @new_title = params[:title]
   @new_url = params[:url]
   @new_description = params[:description]
+
+  
   @articles = Article.all
   @articles_array = []
   @articles.each do |article|
     @articles_array << article.to_s
   end
 
+  # if @new_description.strip.empty? || @new_url.strip.empty? || @new_title.strip.empty?
+  #     #sad_path
+  #     @error = "Please completely fill out form"
+  #     erb :new
+  # elsif  !@new_url.include?("http") 
+  #       @error_http = "Invalid URL"
+  #       erb :new
+  # elsif @new_description.length < 20
+  #       @error_description = "Your description MUST have more than 20 characters. Please try again."
+  #     erb :new
+  #     elsif @articles_array.include?(@new_url.to_s)
+  #       @error_dupe = "Article with same url already submitted"
+  #       erb :new
 
-  if @new_description.strip.empty? || @new_url.strip.empty? || @new_title.strip.empty?
-    #sad_path
-    @error = "Please completely fill out form"
-    erb :new
-elsif  !@new_url.include?("http") 
-      @error_http = "Invalid URL"
-      erb :new
-elsif @new_description.length < 20
-      @error_description = "Your description MUST have more than 20 characters. Please try again."
-     erb :new
-    elsif @articles_array.include?(@new_url.to_s)
-      @error_dupe = "Article with same url already submitted"
-      erb :new
-
-  else
-    #happy path
+  #   else
+  #     #happy path
+  
     @articles = Article.create(@new_title,@new_url,@new_description)
   
     redirect "/articles"
+    
   end
 
 end
